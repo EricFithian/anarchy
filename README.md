7e6420f34804693e92fe0af12956a828194b35f3
EricFithian <ef2413@columbia.edu>
2023-03-29 20:06:51 -0400

    Started working on my controller

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # anarchy
+
+### Causing mayhem all over
+
+We're even adding a controller with our favorite characters for anarchy
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
+const charactersController = require('./controllers/characters')
 
 app.get('/', (req, res) => {
     res.send(`<h1>Hello World! We want anarchy</h1>`)
 })
+
+app.use('', charactersController);
 
 app.listen(PORT, () => {
     console.log(`We're causing mayhem on port ${PORT}`)
